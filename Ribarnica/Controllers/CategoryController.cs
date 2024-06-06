c47725363584970935ded26c920f7a00cf790028
scheiss <zbaric_hr@yahoo.com>
2024-06-06 23:56:27 +0200

    Start

---
+++ Ribarnica/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ribarnica.Data;
+using Ribarnica.Models;
+
+namespace Ribarnica.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext      _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {   
+            var objCategory = _context.Categories.ToList();
+            return View();
+        }
+    }
+}
---
+++ Ribarnica/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ribarnica.Models;
+
+namespace Ribarnica.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        
+
+        
+        {
+                
+        }
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, DisplayOrder = 1, Name = "Slatkovodna Riba" },
+                new Category { Id = 2, DisplayOrder = 2, Name = "Morska Riba" }
+
+
+                );
+           
+
+        }
+    }
+}
